071d9a4d2b0566d84071d33a8e3b85439bee6c66
omas <omas1st@gmail.com>
2025-04-09 14:53:44 +0100

    Update client files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// client/src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Receive from './pages/Receive';
+import Send from './pages/Send';
+import Withdraw from './pages/Withdraw';
+import Buy from './pages/Buy';
+import PaymentPage from './pages/PaymentPage';
+import WithdrawPaymentPage from './pages/WithdrawPaymentPage';
+import SendPaymentPage from './pages/SendPaymentPage';
+import AdminPanel from './pages/AdminPanel';
+import AdminUsers from './pages/AdminUsers';
+import AdminUserSearch from './pages/AdminUserSearch';
+import AdminBankDetails from './pages/AdminBankDetails';
+import AdminNotifications from './pages/AdminNotifications';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/receive" element={<Receive />} />
+        <Route path="/send" element={<Send />} />
+        <Route path="/withdraw" element={<Withdraw />} />
+        <Route path="/buy" element={<Buy />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/withdraw-payment" element={<WithdrawPaymentPage />} />
+        <Route path="/send-payment" element={<SendPaymentPage />} />
+        <Route path="/admin" element={<AdminPanel />} />
+        <Route path="/admin/users" element={<AdminUsers />} />
+        <Route path="/admin/search" element={<AdminUserSearch />} />
+        <Route path="/admin/bank" element={<AdminBankDetails />} />
+        <Route path="/admin/notifications" element={<AdminNotifications />} />
+      </Routes>
+    </Router>
   );
 }
 
